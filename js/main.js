dff9f0daacce9dd0074242d043d422102bf36a6a
Andrey Krasnov <krasnuy@list.ru>
2017-09-27 23:23:02 +0300

    Task-1-3: Начинаем программировать.
    * Показ первого экрана приложения
    * Переключение с клавиатуры экранов на следующий и предыдущий

---
+++ js/main.js
@@ -0,0 +1,105 @@
+// main.js
+
+/**
+ * Главный модуль приложения.
+ * @type {{run}}
+ */
+window.main = (function () {
+  /**
+   * Перечисление кодов клавиш.
+   * @readonly
+   * @enum {number}
+   */
+  const KEY_CODE = {
+    /** Код клавиши "Стрелка влево". */
+    leftArrow: 37,
+    /** Код клавиши "Стрелка вправо". */
+    rightArrow: 39
+  };
+
+  /**
+   * Перечисление номеров экранов.
+   * @readonly
+   * @enum {number}
+   */
+  const SCREEN_NUMBER = {
+    /** Номер неизвестного экрана. */
+    unknown: -1,
+    /** Номер стартового экрана. */
+    start: 0
+  };
+
+  /* Инициализация доступа к DOM элементам.
+   ******************************************************************************/
+
+  const mainContent = document.querySelector(`main.central`);
+  const templates = (function () {
+    const templateNames = [`#greeting`, `#rules`, `#game-1`, `#game-2`, `#game-3`, `#stats`];
+    return templateNames.map((it) => {
+      return document.querySelector(it).content;
+    });
+  }());
+
+  /* Отображение экранов.
+   ******************************************************************************/
+
+  const showScreen = function (screenNumber) {
+    const contentTemplate = templates[screenNumber].cloneNode(true);
+    mainContent.innerHTML = ``;
+    mainContent.appendChild(contentTemplate);
+  };
+
+  /* Управление переключением экранов.
+   ******************************************************************************/
+
+  let currentScreenNumber = SCREEN_NUMBER.unknown;
+
+  const getPreviousScreenNumber = function () {
+    return Math.max(0, currentScreenNumber - 1);
+  };
+
+  const getNextScreenNumber = function () {
+    return Math.min(templates.length - 1, currentScreenNumber + 1);
+  };
+
+  const setCurrentScreenNumber = function (value, callBack) {
+    if (currentScreenNumber !== value) {
+      currentScreenNumber = value;
+      callBack(value);
+    }
+  };
+
+  const changeScreen = function (screenNumber) {
+    setCurrentScreenNumber(screenNumber, (value) => {
+      showScreen(value);
+    });
+  };
+
+  /* Подписка на события DOM.
+   ******************************************************************************/
+
+  const subscribe = function () {
+    document.addEventListener(`keydown`, function (evt) {
+      if (evt.altKey && evt.keyCode === KEY_CODE.leftArrow) {
+        changeScreen(getPreviousScreenNumber());
+      }
+      if (evt.altKey && evt.keyCode === KEY_CODE.rightArrow) {
+        changeScreen(getNextScreenNumber());
+      }
+    });
+  };
+
+  /* Публикация интерфейса модуля.
+   ******************************************************************************/
+
+  return {
+    /** Запускает выполнение приложения. */
+    run: () => {
+      subscribe();
+      setTimeout(() => changeScreen(SCREEN_NUMBER.start), 3000);
+    }
+  };
+
+}());
+
+window.main.run();
